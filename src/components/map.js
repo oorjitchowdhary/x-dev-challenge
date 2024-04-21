8b1622e6107da33fb5b96c6280274817a4671d2f
Oorjit Chowdhary <oorjit.chowdhary@hotmail.com>
2024-04-20 18:12:53 -0700

    circle maps with woeids and print trends api results

--- src/components/map.js
+++ src/components/map.js
@@ -1,13 +1,34 @@
 import React from "react";
-import { MapContainer, TileLayer, useMapEvent } from "react-leaflet";
+import { Circle, MapContainer, TileLayer, useMapEvent } from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
+import latlngs from "../utils/latlngs";
+import woeids from "../utils/woeid";
+
+const handleCircleClick = async (city, latlng) => {
+    console.log(city, latlng);
+    console.log(woeids[city]);
+
+    await fetch(`http://127.0.0.1:5000/api/trends?query=${woeids[city]}`)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+        });
+}
+
+const getCircles = () => {
+    return Object.entries(latlngs).map(([city, latlng]) => {
+        return <Circle center={latlng} pathOptions={{ color: 'red' }} radius={100000} eventHandlers={{ click: () => handleCircleClick(city, latlng) }} />;
+    });
+};
 
 const MapClick = ({ onClick }) => {
     useMapEvent("click", (e) => {
+        console.log(e);
         onClick(e.latlng);
     });
 
     return null;
-}
+};
 
 const Map = () => {
     const handleClick = (latlng) => {
@@ -20,6 +41,7 @@ const Map = () => {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <MapClick onClick={handleClick} />
+            {getCircles()}
         </MapContainer>
     );
 };
